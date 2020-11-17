0f499bdf0fe089cd8fd2691432f0ffa44c94a92b
Zeynep Naz Özşahan <zeyozsahan@Zeynep-Air.local>
2020-11-17 11:00:43 +0100

    replace unreadable with .

--- hexdump.c
+++ hexdump.c
@@ -2,50 +2,55 @@
 
 long calculateBuffSize(long fileSize){      
     long res = 0;
-    while(res <=  fileSize){
-        res += 16 * sizeof(char);
+    while(res < fileSize){
+        res += (16 * sizeof(char));
     }
-    return res+1;
+    return res;
 }
 
 void hexdump(FILE *input, FILE *output) { 
-    //TODO: read String from file
     fseek(input, 0, SEEK_END);
     long fileSize = ftell(input);
     long buffSize = calculateBuffSize(fileSize);
     
     rewind(input);
-
     char arr [buffSize];
-    for(int i=0; i < buffSize; i++)
-	arr[i] = 0;
 
-    fread(arr, 1, fileSize, input);
+    for(int i=0; i < buffSize; i++) //fill buffer with 0s
+	    arr[i] = 0;
+
+    fread(arr, 1, fileSize, input); //read text from file into buffer
     fclose(input);
-    arr[buffSize-1] = 0; //terminate with 0 byte
 
-    int size = sizeof(arr)/(sizeof(arr[0])) -1;
+    int size = sizeof(arr)/(sizeof(arr[0]));
 
     for(int i = 0; i < size; i+=16){
 
-	fprintf(output, "%06x : ", i);
+	    fprintf(output, "%06x : ", i);
 
-	for(int j = 0; j < 16; j++){
-	    if(arr[i+j] == 0){
-		fprintf(output, "   ");
+	    for(int j = 0; j < 16; j++){
+	        if(arr[i+j] == 0){
+		        fprintf(output, "   ");
+	        }   
+	        else{
+		        fprintf(output, "%02x ", arr[i+j]);	
+	        }
 	    }
-	    else{
-		fprintf(output, "%02x ", arr[i+j]);	
+	    printf("  ");
+
+	    for(int k = i; k < i+16; k++){ 
+            //TODO: Nicht darstellbare chars durch . ersetzen
+	        if(arr[k] < 32 || arr[k] > 126)
+                printf(".");
+            else{    
+                if(arr[k] != 0){
+		            fprintf(output, "%c", arr[k]);
+	            }
+            }    
 	    }
-	}
-	printf("  ");
-	for(int k = i; k < i+16; k++){ //TODO: Nicht darstellbare chars durch . ersetzen
-	    if(arr[k] != 0){
-		fprintf(output, "%c", arr[k]);
-	    }
-	}
-	fprintf(output, "\n");  
+	    fprintf(output, "\n");  
     }
+    fclose(output);
     return;
 }
 
