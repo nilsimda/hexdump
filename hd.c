e383544c853ad8700964c87f47a44c43a699805a
Zeynep Naz Özşahan <zeyozsahan@Zeynep-MacBook-Air.fritz.box>
2020-11-24 18:14:21 +0100

    this

--- hd.c
+++ hd.c
@@ -3,13 +3,10 @@
 void hexdump(FILE *input, FILE *output);
 
 int main(int argc, char *argv[]) {
-    printf("%d\n", argc);
 
     for(int i =1; i<argc; i++){
         
-        printf("%s\n", argv[i]);
-        hexdump(fopen(argv[i], "r+" ), stdout);
-        printf("This string");
+       hexdump(fopen(argv[i], "r+" ), stdout);
         
     }
 
